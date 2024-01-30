234bb1e5169050e58f4c4c374bb77c1a6ad469ea
SergeyOsadchiy <mudrec@icloud.com>
2024-01-30 20:58:08 +0300

    Initial Commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,21 +1,31 @@
 function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+   return {
+    min: Math.min (...arr), 
+    max: Math.max(...arr), 
+    avg: Number((arr.reduce((acc, number)=> acc + number,0)/ arr.length).toFixed(2))};
 }
 
 function summElementsWorker(...arr) {
 
+return arr.reduce((acc, number)=> acc + number, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
+  return arr.length === 0 ? 0 : Math.max(...arr) - Math.min(...arr);
 
 }
 
 function differenceEvenOddWorker(...arr) {
+const sumEvenNumbers = arr.filter(item => !item % 2 === 0).reduce((acc, number) => acc + number,0);
+const sumOddNumbers = arr.filter(item => !item % 2 !== 0).reduce((acc, number) => acc + number,0);
 
+return arr.length === 0 ? 0 : sumEvenNumbers - sumOddNumbers;
 }
 
 function averageEvenElementsWorker(...arr) {
+  return arr 
+  .filter(item => !item % 2 === 0)
+  .reduce((acc,number,index,filteredArray) => acc + number / filteredArray.lenger, 0 );
 
 }
 
