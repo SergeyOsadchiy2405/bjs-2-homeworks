b004a99c10e24ffdd966878b1c9a829fba6151b6
SergeyOsadchiy <mudrec@icloud.com>
2024-01-30 21:12:34 +0300

    Initial Commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -30,5 +30,10 @@ function averageEvenElementsWorker(...arr) {
 }
 
 function makeWork (arrOfArr, func) {
-
+  function compareArrays(arr1, arr2) {
+    if (arr1.length===arr2.length && arr1.every((element, index) => element === arr2[index]) ){
+    return true
+    }
+    return false
+    }
 }
